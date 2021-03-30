03e226f16b1fed17e99fe671b0ac96d1e8c2e018
aidan <aidanguarniere@gmail.com>
2021-03-30 09:27:54 -0700

    seeds for User, Reprint, Comment, Followers/Followed and Likes

--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -66,13 +66,16 @@ db.once("open", async () => {
   // create reprints
   let createdReprints = [];
   for (let i = 0; i < 40; i += 1) {
+    const title = faker.name.title()
+    const asset = faker.hacker.noun()
     const caption = faker.lorem.words(Math.round(Math.random() * 20) + 1);
-
+    const marketListing = faker.internet.url()
+    // create author name from a random User's username
     const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
-    const { username, _id: userId } = createdUsers.ops[randomUserIndex];
-
-    const createdThought = await Thought.create({ caption, username });
-
+    const { _id: userId } = createdUsers.ops[randomUserIndex];
+    const { username: author } = createdUsers.ops[randomUserIndex];
+    // const author = JSON.stringify(username)
+    const createdReprint = await Reprint.create({ title, asset, caption, author, marketListing });
     const updatedUser = await User.updateOne(
       { _id: userId },
       { $push: { reprints: createdReprint._id } }
@@ -81,22 +84,40 @@ db.once("open", async () => {
     createdReprints.push(createdReprint);
   }
 
-  await Reprint.collection.insertOne({
-    title: "test title",
-    asset: "test asset",
-    author: "test author",
-    caption: "test caption",
-    marketListing: "https://testmarketlisting.com",
-    likes: [{ _id: "606281d71935576a90a33a66" }],
-    comments: [
-      {
-        commentBody: "test Comment 1",
-        author: "test author 1",
-      },
-    ],
-  });
+  // create comments
+  for (let i = 0; i < 100; i += 1) {
+    const commentBody = faker.lorem.words(Math.round(Math.random() * 20) + 1);
+    // create author name from a random User's username
+    const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
+    const { username: author } = createdUsers.ops[randomUserIndex];
+    // target random Reprint
+    const randomReprintIndex = Math.floor(Math.random() * createdReprints.length);
+    const { _id: reprintId } = createdReprints[randomReprintIndex];
 
-  await Reprint.collection.updateOne({ _id });
+    await Reprint.updateOne(
+      { _id: reprintId },
+      { $push: { comments: { commentBody, author } } },
+      { runValidators: true }
+    );
+  }
+
+  // create likes 
+  for (let i = 0; i < 100; i += 1) {    
+    // get a random User id
+    const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
+    const { _id: userId } = createdUsers.ops[randomUserIndex];
+
+    // add like to random Reprint
+    const randomReprintIndex = Math.floor(Math.random() * createdReprints.length);
+    const { _id: reprintId } = createdReprints[randomReprintIndex];
+
+    await Reprint.updateOne(
+      { _id: reprintId },
+      { $push: { likes: { userId } } },
+      { runValidators: true }
+    );
+  }
+
 
   // Signal to developer that seeding finished
   const bgGreen = "\x1b[32m",
@@ -105,3 +126,20 @@ db.once("open", async () => {
   console.info(printf("%Seeds successful! %s %s"), bgGreen, colorReset);
   process.exit(0);
 });
+
+// await Reprint.collection.insertOne({
+//     title: "test title",
+//     asset: "test asset",
+//     author: "test author",
+//     caption: "test caption",
+//     marketListing: "https://testmarketlisting.com",
+//     likes: [{ _id: "606281d71935576a90a33a66" }],
+//     comments: [
+//       {
+//         commentBody: "test Comment 1",
+//         author: "test author 1",
+//       },
+//     ],
+//   });
+
+//   await Reprint.collection.updateOne({ _id });
