bd8149fb0b0b5fecb48fda9871fa65f7bebe67fb
aidan <aidanguarniere@gmail.com>
2021-04-01 10:48:45 -0700

    removed redundant schemas

--- server/models/User.js
+++ server/models/User.js
@@ -1,22 +1,21 @@
-const { Schema, model } = require('mongoose');
-const bcrypt = require('bcrypt');
+const { Schema, model } = require("mongoose");
 
 // TODO: Please fill in more of the User model. Refer to Aidan's diagrams on Slack or OneNote
 
-const favoriteSchema = new Schema(
-  {
-      reprintId: {
-          type: Schema.Types.ObjectId,
-          ref: "Reprint"
-      },
-  },
-  {
-      toJSON: {
-        getters: true,
-      },
-      id: false,
-    }
-)
+// const favoriteSchema = new Schema(
+//   {
+//     reprintId: {
+//       type: Schema.Types.ObjectId,
+//       ref: "Reprint",
+//     },
+//   },
+//   {
+//     toJSON: {
+//       getters: true,
+//     },
+//     id: false,
+//   }
+// );
 
 const userSchema = new Schema(
   {
@@ -29,7 +28,7 @@ const userSchema = new Schema(
       type: String,
       required: true,
       unique: true,
-      match: [/.+@.+\..+/, 'Must use a valid email address'],
+      match: [/.+@.+\..+/, "Must use a valid email address"],
     },
     password: {
       type: String,
@@ -38,52 +37,57 @@ const userSchema = new Schema(
     reprints: [
       {
         type: Schema.Types.ObjectId,
-        ref: 'Reprint'
-      }
+        ref: "Reprint",
+      },
     ],
-    favorites: [favoriteSchema],
+    favorites: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Reprint",
+      },
+    ],
     followers: [
       {
         type: Schema.Types.ObjectId,
-        ref: 'User'
-      }
+        ref: "User",
+      },
     ],
     followed: [
       {
         type: Schema.Types.ObjectId,
-        ref: 'User'
-      }
-    ]
+        ref: "User",
+      },
+    ],
   },
   // set this to use virtual below
   {
     toJSON: {
       virtuals: true,
-      getters: true
+      getters: true,
     },
   }
 );
 
 // custom method to compare and validate password for logging in
 userSchema.methods.isCorrectPassword = async function (password) {
-  return password===this.password;
+  return password === this.password;
 };
 
 // count followers
-userSchema.virtual('followerCount').get(function() {
+userSchema.virtual("followerCount").get(function () {
   return this.followers.length;
 });
 
 // count followed
-userSchema.virtual('followedCount').get(function() {
+userSchema.virtual("followedCount").get(function () {
   return this.followed.length;
 });
 
 // count favorites
-userSchema.virtual('favoriteCount').get(function() {
+userSchema.virtual("favoriteCount").get(function () {
   return this.favorites.length;
 });
 
-const User = model('User', userSchema);
+const User = model("User", userSchema);
 
 module.exports = User;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,6 +5,8 @@ const typeDefs = gql `
     _id: ID
     username: String
     email: String
+    reprints: [Reprint]
+    favorites: [Reprint]
   }
 
   type Reprint {
