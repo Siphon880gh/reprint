6f520f2ad9d1e737a5e44f58209a41f85ad40bca
Jonathan Wilferd <jwilferd10@yahoo.com>
2021-04-01 22:40:45 -0700

    Feat: addPost has form elements added

--- client/src/pages/addPost.js
+++ client/src/pages/addPost.js
@@ -5,6 +5,51 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 // Create a const for postForm that'll return JSX
+const postForm = () => {
 
+    // Return JSX
+    return (
+        <div>
+            <Form>
+
+                {/* Choose File Button */}
+                <Form.Group>
+                    <Form.File id="chooseFile" label="Import A File:" />
+                </Form.Group>
+
+                {/* Add A Title */}
+                <Form.Group controlId="titleInput">
+                    <Form.Label>Add A Title:</Form.Label>
+                    <Form.Control/>
+                </Form.Group>
+
+                {/* Add An Author */}
+                <Form.Group controlId="authorInput">
+                    <Form.Label>Add An Author:</Form.Label>
+                    <Form.Control/>
+                </Form.Group>
+
+                {/* Add Market Listing */}
+                <Form.Group controlId="marketListing">
+                    <Form.Label>Add Market Listing:</Form.Label>
+                    <Form.Control placeholder="https://www.example.com/" />
+                </Form.Group>
+
+
+                {/* Captions */}
+                <Form.Group controlId="captionInput">
+                    <Form.Label>Add A Caption:</Form.Label>
+                    <Form.Control as="textarea" placeholder="Optional: Add a caption!"rows={3} />
+                </Form.Group>
+
+                {/* Submit */}
+                <Button variant="primary" type="submit">
+                    Post
+                </Button>
+
+            </Form>
+        </div>
+    )
+}
 // Export addPost
 export default postForm;
