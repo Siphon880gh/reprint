699108b055f35de3a8650e242ab5048a31edb13c
Jonathan Wilferd <jwilferd10@yahoo.com>
2021-04-01 22:39:04 -0700

    Feat: Basics of the page being added

---
+++ client/src/pages/addPost.js
@@ -0,0 +1,10 @@
+// Create a skeletal structure of what our addPost page will look like
+// Set up imports at the top
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+// Create a const for postForm that'll return JSX
+
+// Export addPost
+export default postForm;
