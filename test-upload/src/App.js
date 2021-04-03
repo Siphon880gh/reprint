ad7accd4065aa65d2ca08545840db2f218b0afda
Weng Fei Fung <weffung@ucdavis.edu>
2021-04-02 19:10:13 -0700

    Basic component rendering

---
+++ test-upload/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import UploadForm from './UploadForm.js'
+
+function App() {
+  return (
+    <main className="App">
+      <UploadForm/>    
+    </main>
+  );
+}
+
+export default App;
---
+++ test-upload/src/UploadForm.js
@@ -0,0 +1,7 @@
+export default function UploadForm(props) {
+    return (
+        <div>
+            Upload Form
+        </div>
+    )
+}
