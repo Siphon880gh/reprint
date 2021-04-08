951a98f75c1bc1d53efd950d0a5eb6dd395f4a8a
CassBut <clhurlbut@gmail.com>
2021-04-08 14:23:53 -0700

    changed the service worker around to proper places following the cra template

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import App from './App';
 
 ReactDOM.render(
@@ -10,3 +11,9 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
