6e77687e47097df5764c917ecbe044f0c9d56064
Weng Fei Fung <weffung@ucdavis.edu>
2021-04-10 16:07:04 -0700

    Removed GraphQL caching, but continued caching static files

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -20,11 +20,3 @@ self.addEventListener("install", event => {
         })
     );
 });
-
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
-});
