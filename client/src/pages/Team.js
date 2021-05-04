9e1c7972e0c7fb9293aafb261f4b66fa48ea0b24
Weng Fei Fung <weffung@ucdavis.edu>
2021-05-04 16:04:01 -0700

    Removed class attribute for className

--- client/src/pages/Team.js
+++ client/src/pages/Team.js
@@ -38,7 +38,7 @@ const MeetTheTeam = () => {
                     <Figure.Caption>
                         <p><a href="https://github.com/AidanGuarniere" target="_blank" rel="noopener noreferrer">Github</a></p>
                         <p><a href="https://www.linkedin.com/in/aidan-guarniere-56299719b/" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
-                        <p class="desc-team-member">I like gardening, long walks on the beach, and MERN stack JavaScript. You can probably find me working on some code with a questionable amount of coffee nearby.</p>
+                        <p className="desc-team-member">I like gardening, long walks on the beach, and MERN stack JavaScript. You can probably find me working on some code with a questionable amount of coffee nearby.</p>
                     </Figure.Caption>
                 </Figure>
 
@@ -53,8 +53,8 @@ const MeetTheTeam = () => {
                     <Figure.Caption>
                         <p><a href="https://github.com/clhurlbut" target="_blank" rel="noopener noreferrer">Github</a></p>
                         <p><a href="https://www.linkedin.com/in/cassandra-hurlbut-7165081b6/" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
-                        <p class="desc-team-member">Bachelors of Fine Arts in Cinema Screenwriting, certified Professional Association of Therapeutic Horsemanship Riding Instructor, certified Equine Assisted Growth And Learning Association Equine Specialist,</p>
-                        <p class="desc-team-member">and now certified creative coding developer! Master of merging imagination and fun with practical application</p>
+                        <p className="desc-team-member">Bachelors of Fine Arts in Cinema Screenwriting, certified Professional Association of Therapeutic Horsemanship Riding Instructor, certified Equine Assisted Growth And Learning Association Equine Specialist,</p>
+                        <p className="desc-team-member">and now certified creative coding developer! Master of merging imagination and fun with practical application</p>
                     </Figure.Caption>
                 </Figure>
 
@@ -69,8 +69,8 @@ const MeetTheTeam = () => {
                     <Figure.Caption>
                         <p><a href="https://github.com/jwilferd10" target="_blank" rel="noopener noreferrer">Github</a></p>
                         <p><a href="https://www.linkedin.com/in/jonathan-wilferd-3a6b461b6/" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
-                        <p class="desc-team-member">Always striving to learn and understand how code works and continously striving to push myself out of my comfort zone to learn those new concepts. Took up a course at UCLA's Coding Bootcamp and am working</p>
-                        <p class="desc-team-member">to become a better developer! </p>
+                        <p className="desc-team-member">Always striving to learn and understand how code works and continously striving to push myself out of my comfort zone to learn those new concepts. Took up a course at UCLA's Coding Bootcamp and am working</p>
+                        <p className="desc-team-member">to become a better developer! </p>
                     </Figure.Caption>
                 </Figure>
 
@@ -86,8 +86,8 @@ const MeetTheTeam = () => {
                         <p><a href="https://github.com/Siphon880gh/" target="_blank" rel="noopener noreferrer">Github</a></p>
                         <p><a href="https://www.linkedin.com/in/weng-fung" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
                         <p><a href="https://www.youtube.com/user/Siphon880yt/videos" target="_blank" rel="noopener noreferrer">Youtube</a></p>
-                        <p class="desc-team-member">Passionate about all things coding especially web development, Weng has made this a hobby and side job for countless years.</p>
-                        <p class="desc-team-member">Have worked with startups including Mixotype, companies like ExRx, and small gigs by word of mouth. He graduated from UCLA-Trilogy Coding Bootcamp on 4/8/2021.</p>
+                        <p className="desc-team-member">Passionate about all things coding especially web development, Weng has made this a hobby and side job for countless years.</p>
+                        <p className="desc-team-member">Have worked with startups including Mixotype, companies like ExRx, and small gigs by word of mouth. He graduated from UCLA-Trilogy Coding Bootcamp on 4/8/2021.</p>
                     </Figure.Caption>
                 </Figure>
             </section>
