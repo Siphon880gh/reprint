a3176398ded437eb06eb6fc18a7699a62c8717ef
Weng Fei Fung <weffung@ucdavis.edu>
2021-05-04 17:08:31 -0700

    Visually cleaner Team member bio description

--- client/src/pages/Team.js
+++ client/src/pages/Team.js
@@ -15,9 +15,12 @@ const MeetTheTeam = () => {
             img: Aidan,
             html: ()=>(
                 <React.Fragment>
-                    <p><a href="https://github.com/AidanGuarniere" target="_blank" rel="noopener noreferrer">Github</a></p>
-                    <p><a href="https://www.linkedin.com/in/aidan-guarniere-56299719b/" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
                     <p className="desc-team-member">I like gardening, long walks on the beach, and MERN stack JavaScript. You can probably find me working on some code with a questionable amount of coffee nearby.</p>
+                    <p>
+                        <a href="https://github.com/AidanGuarniere" target="_blank" rel="noopener noreferrer">Github</a>
+                        <span>&nbsp;|&nbsp;</span>
+                        <a href="https://www.linkedin.com/in/aidan-guarniere-56299719b/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+                    </p>
                 </React.Fragment>
             )
         },
@@ -26,10 +29,13 @@ const MeetTheTeam = () => {
             img: Cassandra,
             html: ()=>(
                 <React.Fragment>
-                    <p><a href="https://github.com/clhurlbut" target="_blank" rel="noopener noreferrer">Github</a></p>
-                    <p><a href="https://www.linkedin.com/in/cassandra-hurlbut-7165081b6/" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
-                    <p className="desc-team-member">Bachelors of Fine Arts in Cinema Screenwriting, certified Professional Association of Therapeutic Horsemanship Riding Instructor, certified Equine Assisted Growth And Learning Association Equine Specialist,</p>
+                    <p className="desc-team-member">Bachelors of Fine Arts in Cinema Screenwriting, certified Professional Association of Therapeutic Horsemanship Riding Instructor, certified Equine Assisted Growth And Learning Association Equine Specialist.</p>
                     <p className="desc-team-member">and now certified creative coding developer! Master of merging imagination and fun with practical application</p>
+                    <p>
+                        <a href="https://github.com/clhurlbut" target="_blank" rel="noopener noreferrer">Github</a>
+                        <span>&nbsp;|&nbsp;</span>
+                        <a href="https://www.linkedin.com/in/cassandra-hurlbut-7165081b6/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+                    </p>
                 </React.Fragment>
             )
         },
@@ -38,23 +44,30 @@ const MeetTheTeam = () => {
             img: Jonathan,
             html: ()=>(
                 <React.Fragment>
-                    <p><a href="https://github.com/jwilferd10" target="_blank" rel="noopener noreferrer">Github</a></p>
-                    <p><a href="https://www.linkedin.com/in/jonathan-wilferd-3a6b461b6/" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
                     <p className="desc-team-member">Always striving to learn and understand how code works and continously striving to push myself out of my comfort zone to learn those new concepts. Took up a course at UCLA's Coding Bootcamp and am working</p>
                     <p className="desc-team-member">to become a better developer! </p>
+                    <p>
+                        <a href="https://github.com/jwilferd10" target="_blank" rel="noopener noreferrer">Github</a>
+                        <span>&nbsp;|&nbsp;</span>
+                        <a href="https://www.linkedin.com/in/jonathan-wilferd-3a6b461b6/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+                    </p>
                 </React.Fragment>
             )
         },
         {
-            name: "Jonathan Wilferd",
-            img: Jonathan,
+            name: "Weng Fei Fung",
+            img: Weng,
             html: ()=>(
                 <React.Fragment>
-                    <p><a href="https://github.com/Siphon880gh/" target="_blank" rel="noopener noreferrer">Github</a></p>
-                    <p><a href="https://www.linkedin.com/in/weng-fung" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
-                    <p><a href="https://www.youtube.com/user/Siphon880yt/videos" target="_blank" rel="noopener noreferrer">Youtube</a></p>
                     <p className="desc-team-member">Passionate about all things coding especially web development, Weng has made this a hobby and side job for countless years.</p>
                     <p className="desc-team-member">Have worked with startups including Mixotype, companies like ExRx, and small gigs by word of mouth. He graduated from UCLA-Trilogy Coding Bootcamp on 4/8/2021.</p>
+                    <p>
+                        <a href="https://github.com/Siphon880gh/" target="_blank" rel="noopener noreferrer">Github</a>
+                        <span>&nbsp;|&nbsp;</span>
+                        <a href="https://www.linkedin.com/in/weng-fung" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+                        <span>&nbsp;|&nbsp;</span>
+                        <a href="https://www.youtube.com/user/Siphon880yt/videos" target="_blank" rel="noopener noreferrer">Youtube</a>
+                    </p>
                 </React.Fragment>
             )
         }
@@ -82,7 +95,7 @@ const MeetTheTeam = () => {
                         </Figure>
                     </React.Fragment>
                 )
-            })};
+            })}
             </section>
 
         </div>
